9001a8e4d4db88cbd10f136cc3f6fd0aae4e8e79
Teofanus Gary Setiawan <garyteofanus@localhost.localdomain>
2020-05-21 00:57:41 +0700

    First commit

---
+++ src/main/java/com/project/pokeproject/MainController.java
@@ -0,0 +1,53 @@
+package com.project.pokeproject;
+
+import com.project.pokeproject.model.InputPokemon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+// import eu.iamgio.pokedex.pokemon.Pokemon;
+// import eu.iamgio.pokedex.pokemon.PokemonAbility;
+// import eu.iamgio.pokedex.pokemon.PokemonType;
+
+// import eu.iamgio.pokedex.util.Pair;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/form")
+    public String formInputPokemon(Model model) {
+        model.addAttribute("pokemon", new InputPokemon());
+        return "form";
+    }
+
+    @PostMapping("/form")
+    public String submitFormInputPokemon(@ModelAttribute InputPokemon pokemon) {
+        return "form-result";
+    }
+
+    // public static void main(String[] args) {
+    //     Pokemon bulbasaur = Pokemon.fromName("bulbasaur");
+    //     Pokemon ditto = Pokemon.fromName("ditto");
+    //     Pokemon charmander = Pokemon.fromName("charmander");
+
+    //     Pair<PokemonType, PokemonType> type_charmander = charmander.getTypes();
+    //     System.out.println(type_charmander.getFirst());
+    //     System.out.println(type_charmander.getSecond());
+
+    //     PokemonAbility ditto_ability = PokemonAbility.fromName("imposter");
+    //     System.out.println(ditto_ability);
+
+    //     Pair<PokemonType, PokemonType> types = bulbasaur.getTypes();
+    //     System.out.println(types.getFirst());
+    //     System.out.println(types.getSecond());
+    // }
+}
