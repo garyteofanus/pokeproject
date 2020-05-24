5467b056981a1ecac02c870e319bb8125f686783
Antonius Anggito Arissaputro <antoniusanggito@gmail.com>
2020-05-24 11:04:47 +0700

    Debug calling variable (not fixed)

--- src/main/java/com/project/pokeproject/MainController.java
+++ src/main/java/com/project/pokeproject/MainController.java
@@ -2,24 +2,27 @@ package com.project.pokeproject;
 
 import com.project.pokeproject.model.InputPokemon;
 
+import eu.iamgio.pokedex.pokemon.Stat;
+import eu.iamgio.pokedex.pokemon.move.PokemonPersonalMove;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-// import eu.iamgio.pokedex.pokemon.Pokemon;
-// import eu.iamgio.pokedex.pokemon.PokemonAbility;
-// import eu.iamgio.pokedex.pokemon.PokemonType;
-
-// import eu.iamgio.pokedex.util.Pair;
+import eu.iamgio.pokedex.pokemon.Pokemon;
+import eu.iamgio.pokedex.pokemon.PokemonAbility;
+import eu.iamgio.pokedex.pokemon.PokemonType;
+import eu.iamgio.pokedex.util.Pair;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
     public String index(Model model) {
+        Pokemon zorua = Pokemon.fromName("zorua");
+        model.addAttribute("namaPokemon", zorua.getName());
+        model.addAttribute(zorua);
         return "index";
     }
 
@@ -34,20 +37,14 @@ public class MainController {
         return "form-result";
     }
 
-    // public static void main(String[] args) {
-    //     Pokemon bulbasaur = Pokemon.fromName("bulbasaur");
-    //     Pokemon ditto = Pokemon.fromName("ditto");
-    //     Pokemon charmander = Pokemon.fromName("charmander");
-
-    //     Pair<PokemonType, PokemonType> type_charmander = charmander.getTypes();
-    //     System.out.println(type_charmander.getFirst());
-    //     System.out.println(type_charmander.getSecond());
-
-    //     PokemonAbility ditto_ability = PokemonAbility.fromName("imposter");
-    //     System.out.println(ditto_ability);
-
-    //     Pair<PokemonType, PokemonType> types = bulbasaur.getTypes();
-    //     System.out.println(types.getFirst());
-    //     System.out.println(types.getSecond());
-    // }
+//    public static void main(String[] args) {
+//        Pokemon zorua = Pokemon.fromName("zorua");
+//        System.out.printf("You caught a %s!\n", zorua.getSpeciesName());
+//        Pair<PokemonType, PokemonType> type_zorua = zorua.getTypes();
+//        System.out.println(zorua.getStat(Stat.Type.SPECIAL_ATTACK));
+//        System.out.printf("Type : %s, %s", type_zorua.getFirst(), type_zorua.getSecond());
+//
+//        PokemonAbility ditto_ability = PokemonAbility.fromName("imposter");
+//        System.out.println(ditto_ability);
+//    }
 }
--- src/main/java/com/project/pokeproject/model/InputPokemon.java
+++ src/main/java/com/project/pokeproject/model/InputPokemon.java
@@ -1,50 +1,22 @@
 package com.project.pokeproject.model;
 
-import eu.iamgio.pokedex.pokemon.Pokemon;
-import eu.iamgio.pokedex.pokemon.PokemonType;
-import eu.iamgio.pokedex.util.Pair;
-
 public class InputPokemon {
+
     private String name;
-    private PokemonType type1;
-    private PokemonType type2;
 
     /**
-     * @return the name
+     * Getter method for name variable.
+     * @return String name
      */
     public String getName() {
         return this.name;
     }
 
     /**
-     * @param name the name to set
+     * Setter method for name variable.
      */
     public void setName(String name) {
         this.name = name;
     }
 
-    /**
-     * @return the type1
-     */
-    public PokemonType getType1() {
-        return this.type1;
-    }
-
-    /**
-     * @return the type2
-     */
-    public PokemonType getType2() {
-        return this.type2;
-    }
-
-    public Pokemon createPokemon() {
-        return Pokemon.fromName(name.toLowerCase());
-    }
-
-    public void getPokemonType() {
-        Pair<PokemonType, PokemonType> types = createPokemon().getTypes();
-        this.type1 = types.getFirst();
-        this.type2 = types.getSecond();
-    }
-    
 }
